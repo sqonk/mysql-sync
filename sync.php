0c76f0cce001115baf54a548686694b23b14c36b
Theo Howell <sqonk@sqonk.com.au>
2026-01-31 09:46:00 +1100

    Code formatter.

--- sync.php
+++ sync.php
@@ -1,31 +1,30 @@
 <?php
 
 use sqonk\phext\core\arrays;
-//use sqonk\phext\core\strings;
+// use sqonk\phext\core\strings;
 use sqonk\phext\context\context;
 
 // Run a synchronisation using a json config file.
 function mysql_sync_with_conf(string $filePath): void
 {
-  if (! $filePath or ! str_ends_with(haystack: $filePath, needle: '.json')) {
-    println("This script should take only 1 argument, being a json file containing both the source and destination database information. Examine the database.sample.json file for an example.");
+  if (!$filePath or !str_ends_with(haystack: $filePath, needle: '.json')) {
+    println('This script should take only 1 argument, being a json file containing both the source and destination database information. Examine the database.sample.json file for an example.');
     exit;
   }
-  if (! file_exists($filePath)) {
-    println("The specified file does not exist.");
+  if (!file_exists($filePath)) {
+    println('The specified file does not exist.');
     exit;
   }
 
   mysql_sync(json_decode(file_get_contents($filePath)));
 }
 
-
 /**
- * Run a synchronsisation using an in-memory configuration object. The object should be in 
+ * Run a synchronsisation using an in-memory configuration object. The object should be in
  * the same structure as the sample json file.
- * 
+ *
  * An array can also be provided, which will be converted to an object internally.
- * 
+ *
  * @param object|array<mixed> $config
  */
 function mysql_sync(object|array $config): void
@@ -34,13 +33,13 @@ function mysql_sync(object|array $config): void
     $config = json_decode(json_encode($config));
   }
 
-  # --- Connect to both databases.
+  // --- Connect to both databases.
   println("\n--Source: {$config->source->user}@{$config->source->host}/{$config->source->database}");
 
   $source = new mysqli($config->source->host, $config->source->user, $config->source->password, $config->source->database, $config->source->port);
-  if (! $source || $source->connect_error) { // @phpstan-ignore-line
-    println("Unable to connect to the source MySQL database.");
-    if ($err = $source->connect_error) { // @phpstan-ignore-line
+  if (!$source || $source->connect_error) {  // @phpstan-ignore-line
+    println('Unable to connect to the source MySQL database.');
+    if ($err = $source->connect_error) {  // @phpstan-ignore-line
       println($err);
     }
     exit;
@@ -49,24 +48,24 @@ function mysql_sync(object|array $config): void
   println("\n--Dest: {$config->dest->user}@{$config->dest->host}/{$config->dest->database}\n");
 
   $dest = new mysqli($config->dest->host, $config->dest->user, $config->dest->password, $config->dest->database, $config->dest->port);
-  if (!$source || $source->connect_error) { // @phpstan-ignore-line
-    println("Unable to connect to the source MySQL database.");
-    if ($err = $source->connect_error) { // @phpstan-ignore-line
+  if (!$source || $source->connect_error) {  // @phpstan-ignore-line
+    println('Unable to connect to the source MySQL database.');
+    if ($err = $source->connect_error) {  // @phpstan-ignore-line
       println($err);
     }
     exit;
   }
 
-  $ignoreColumnWidths = (bool)$config->ignoreColumnWidths;
+  $ignoreColumnWidths = (bool) $config->ignoreColumnWidths;
 
-  # ---- Run the sync process, first as a dry run to see what has changed and then ask the user if they want to do it for real.
-  println("Displaying differences..");
+  // ---- Run the sync process, first as a dry run to see what has changed and then ask the user if they want to do it for real.
+  println('Displaying differences..');
 
   $statements = sync($source, $dest, true, $ignoreColumnWidths);
   if (getCounts($statements) > 0) {
     do {
-      $r = ask("Do you want to deploy the changes to the destination, dump the SQL modification commands to a file or cancel? [D]eploy to destination, [s]ave to file, [c]ancel");
-    } while (! arrays::contains(['D', 's', 'c'], $r));
+      $r = ask('Do you want to deploy the changes to the destination, dump the SQL modification commands to a file or cancel? [D]eploy to destination, [s]ave to file, [c]ancel');
+    } while (!arrays::contains(['D', 's', 'c'], $r));
 
     if ($r == 'D') {
       context::mysql_transaction($dest)->do(function ($dest) use ($source, $ignoreColumnWidths) {
@@ -81,15 +80,15 @@ function mysql_sync(object|array $config): void
 }
 
 /**
- * @return array<string, mixed> 
+ * @return array<string, mixed>
  */
 function describe(mysqli $db, bool $ignoreColumnWidths): array
 {
   $tables = [];
 
-  foreach ($db->query("SHOW TABLES") as $row) {
+  foreach ($db->query('SHOW TABLES') as $row) {
     $tableName = arrays::first($row) ?? '';
-    if (! $tableName) {
+    if (!$tableName) {
       continue;
     }
 
@@ -106,7 +105,7 @@ function describe(mysqli $db, bool $ignoreColumnWidths): array
 
         $type = $info['Type'] ?? '';
         if ($ignoreColumnWidths && $type && str_contains(haystack: $type, needle: '(')) {
-          $info['Type'] = preg_replace("/(\(.+?\))/", "", $type);
+          $info['Type'] = preg_replace('/(\(.+?\))/', '', $type);
         }
 
         $name = $info['Field'];
@@ -140,7 +139,6 @@ function sync(mysqli $source, mysqli $dest, bool $dryRun, bool $ignoreColumnWidt
   $dropped = array_diff_key($dest_tables, $source_tables);
   $existing = array_diff_key($source_tables, $new);
 
-
   $newStatements = [];
   $dropStatements = [];
   $alterStatements = [];
@@ -149,9 +147,9 @@ function sync(mysqli $source, mysqli $dest, bool $dryRun, bool $ignoreColumnWidt
   foreach ($new as $tblName => $cols) {
     $create = $source->query("SHOW CREATE TABLE `$tblName`");
     if ($create && $r = $create->fetch_assoc()) {
-      $create = $r["Create Table"];
+      $create = $r['Create Table'];
       $newStatements[] = $create;
-      if (! $dryRun) {
+      if (!$dryRun) {
         println("creating $tblName");
         $dest->query($create);
       } else {
@@ -168,7 +166,7 @@ function sync(mysqli $source, mysqli $dest, bool $dryRun, bool $ignoreColumnWidt
     foreach ($dropped as $tblName => $cols) {
       $drop = "DROP TABLE `$tblName`";
       $dropStatements[] = $drop;
-      if (! $dryRun) {
+      if (!$dryRun) {
         println("dropping $tblName");
         $dest->query($drop);
       } else {
